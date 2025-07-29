8de086041cced1e85e116743d42a7d858c6a3ceb
MaximoQuin <169080723+MaximoQuin@users.noreply.github.com>
2025-07-29 12:52:41 -0600

    add cors

--- app/main.py
+++ app/main.py
@@ -1,9 +1,20 @@
-# app/main.py
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware # Importar CORSMiddleware
 from app.routes import router
 
 app = FastAPI(title="Fútbol Predictor", version="1.0.0")
 
-# Ya no necesitamos hooks de startup ni imports de train_global_* que ya no existen
+origins = [
+    "http://localhost",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router)
